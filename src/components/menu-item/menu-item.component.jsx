e9c7fdf1211d79d67e45b362f8d37e9e6430dcf6
Jason Navarro <Jasonnavarro86@yahoo.com>
2020-01-28 10:52:38 -0500

    Updating out app page, collection preview component and collection-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom'
 import './menu-item.styles.scss'
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+
     <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`) }>
       <div style={{backgroundImage:`url(${imageUrl})`}}  className="background-image"></div>
         <div className="content">
@@ -10,6 +11,7 @@ const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
             <span className="subtitle" > SHOP NOW</span>
         </div>
     </div>
+    
 )
 
 export default withRouter(MenuItem);
